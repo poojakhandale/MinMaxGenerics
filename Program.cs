b9651ad30d29a5b3b93b60875b3030bbb15971a5
poojakhandale <poojakhandale2000@gmail.com>
2023-02-24 20:25:02 +0530

    Greeting Massage

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace MaxMinGenerics;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Day 13 Practice Problems --> CompareTo() with Generics");
+
+    }
+}
+
+
+
+ 
