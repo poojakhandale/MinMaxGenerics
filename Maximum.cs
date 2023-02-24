2537cc9fa1aff1217a3a56664e9146fa5e4c0f3d
poojakhandale <poojakhandale2000@gmail.com>
2023-02-24 21:41:34 +0530

    float maximum number

--- Maximum.cs
+++ Maximum.cs
@@ -28,6 +28,27 @@ namespace MaxMinGenerics
             }
             return a;
         }
+        public static double MaximumFloatNumber(double a, double b, double c)
+        {
+            if (a.CompareTo(b) > 0 && a.CompareTo(c) > 0 || a.CompareTo(b) >= 0 && a.CompareTo(c) > 0 || a.CompareTo(b) > 0 && a.CompareTo(c) >= 0)
+            {
+                return a;
+            }
+
+            if (b.CompareTo(a) > 0 && b.CompareTo(c) > 0 || b.CompareTo(a) >= 0 && b.CompareTo(c) > 0 || b.CompareTo(a) > 0 && b.CompareTo(c) >= 0)
+            {
+                return b;
+            }
+
+            if (c.CompareTo(a) > 0 && c.CompareTo(b) > 0 || c.CompareTo(a) >= 0 && c.CompareTo(b) > 0 || c.CompareTo(a) > 0 && c.CompareTo(b) >= 0)
+            {
+                return c;
+
+            }
+            return a;
+        }
     }
 }
+    
+
 
--- Program.cs
+++ Program.cs
@@ -6,10 +6,24 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Day 13 Practice Problems --> CompareTo() with Generics");
+        Console.WriteLine("Maximum using CompareTo() Mathod");
+        Console.WriteLine("Enter 1st Number: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter 2nd Number: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter 3rd Number: ");
+        int c = Convert.ToInt32(Console.ReadLine());
+
+
+        int max = Maximum.MaximumNumber(a, b, c);
+
+        Console.WriteLine("The maximum of the given three numbers is : " + max);
 
     }
+
+
 }
+
 
 
 
