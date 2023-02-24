ef971b917df96416baa82176e66349be0db8cf3b
poojakhandale <poojakhandale2000@gmail.com>
2023-02-24 21:54:09 +0530

    Taking three string value to compare

--- Program.cs
+++ Program.cs
@@ -8,21 +8,24 @@ public class Program
     {
         Console.WriteLine("Maximum using CompareTo() Mathod");
         Console.WriteLine("Enter 1st Number: ");
-        int a = Convert.ToInt32(Console.ReadLine());
+        double a = Convert.ToDouble(Console.ReadLine());
         Console.WriteLine("Enter 2nd Number: ");
-        int b = Convert.ToInt32(Console.ReadLine());
+        double b = Convert.ToDouble(Console.ReadLine());
         Console.WriteLine("Enter 3rd Number: ");
-        int c = Convert.ToInt32(Console.ReadLine());
+        double c = Convert.ToDouble(Console.ReadLine());
 
 
-        int max = Maximum.MaximumNumber(a, b, c);
+
+        double max = Maximum.MaximumFloatNumber(a, b, c);
 
         Console.WriteLine("The maximum of the given three numbers is : " + max);
 
     }
-
-
 }
+
+
+
+
 
 
 
